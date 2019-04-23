ea77cff040be7b024e77002ef72ad3931324fcdf
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-04-23 19:56:47 +0100

    use connect to to get data from redux store into component. Pass mapStateToProps with the state items we want (songs) to connect and the Component (SongList) we want the props to be available in

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,10 +1,30 @@
 //import React from "react";
 import React, { Component } from "react"; // alternate way of importing Component base class
+import { connect } from "react-redux";
 
 class SongList extends Component {
   render() {
+    console.log(this.props);
     return <div>SongList</div>;
   }
 }
 
-export default SongList;
+const mapStateToProps = state => {
+  return { songs: state.songs };
+
+  //songs will show up as props in SongList component
+};
+
+//create an instance of connect component and pass it some configuration that will get it to reach back up to the
+//Provider and tell it that it wants to get a list of all the songs inside of our app.
+export default connect(mapStateToProps)(SongList);
+
+/*
+function connect(){
+    return function(){
+        console.log('hello');
+    }
+}
+
+connect()();
+*/
