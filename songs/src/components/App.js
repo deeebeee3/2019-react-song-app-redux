b8b7952f1576a90c55986a122ba58c39fd71d1c3
Deepak Bhari <deepak.bhari1@Deepaks-MacBook-Pro.local>
2019-04-23 23:27:02 +0100

    a bit more styling - grid row column

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -9,8 +9,12 @@ Redux for managing state...
 
 const App = () => {
   return (
-    <div>
-      <SongList />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 };
