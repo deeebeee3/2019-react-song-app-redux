89f299b89ec43f0acb33fe8c716dacd482f167e4
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-04-23 17:58:45 +0100

    create initial functional App component

---
+++ songs/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+/*
+Not going to use component level state for this project so make 
+App a functional rather than class based component... Instead going to use
+Redux for managing state...
+*/
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
