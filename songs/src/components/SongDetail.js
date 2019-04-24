7278580df565094d87fffbe20a7b2e7b89325f19
Deepak Bhari <deepak.bhari1@Deepaks-MBP.default>
2019-04-24 12:50:04 +0100

    destructure song out of props. fix no song selected error on application load and prompt user to select one

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = props => {
-  console.log(props);
-  return <div>SongDetail</div>;
+const SongDetail = ({ song }) => {
+  if(!song){
+      return <div>Select a song</div>
+  }
+
+  return <div>{song.title}</div>;
 };
 
+//Will be rerun whenever state updated - so component will have up to date state available through props
 const mapStateTopProps = state => {
   return { selectedSong: state.selectedSong };
 };
 
+//All components hooked up to the connect function will automatically rerender whenever state updated
 export default connect(mapStateTopProps)(SongDetail);
